4f4c036969be9b5c6f6a6455db2bb3d3a3e826cb
AlynxNeko <109527959+AlynxNeko@users.noreply.github.com>
2023-12-14 21:03:03 +0700

    Landing page done
    yeee aga jele bawahnya tapi

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bouken Da Bouken</title>
+    <?php
+    session_start();
+    header('Location: page/index.php');
+    ?>
 </head>
 <body>
     <p>Bouken</p>
---
+++ page/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./Assets/TavecLogo.gif" type="image/x-icon">
+    <title>Tavec</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/main.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class="parallax">
+      
+      <header class="primary-header">
+        <div class="wrapper">
+          <div class="primary-header__inner">
+            <div class="logo">
+              <img id="name" src="./Assets/TavecLogo.svg" alt="" />
+            </div>
+            <nav class="primary-nav">
+              <ul role="list">
+                <li><a href="#faq">FAQ</a></li>
+                <li><a href="../website/index.php">Login</a></li>
+                <li><a href="../website/register.php">Get Started</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div class="hero">
+        <div class="wrapper">
+          <h1 class="hero__title">
+            <span>Gear up!</span> We're going <div class = "a">to make money!</div>
+          </h1>
+          <h3>Free to use!</h3>
+        </div>
+      </div>
+
+      <img class="parallax__bg" src="Assets/building.png" alt="" />
+      <img class="parallax__dust" src="public/assets/dust.webp" alt="" />
+      <img
+        class="parallax__foreground-back"
+        src="public/assets/foreground-back.webp"
+        alt=""
+      />
+      <img
+        class="parallax__foreground-front"
+        src="public/assets/foreground-front.webp"
+        alt=""
+      />
+      <img class="parallax__jax" src="Assets/Elon-Musk.png" alt="" />
+      <img class="parallax__rays" src="Assets/Jeff-Bezos.png" alt="" />
+      <!-- <img class="parallax__mone1" height="auto" src="Assets/money1.png" alt="" /> -->
+      <!-- <img class="parallax__mone2" height="auto" src="Assets/money2.png" alt="" /> -->
+      <!-- <img class="parallax__mone3" height="auto" src="Assets/money3.png" alt="" /> -->
+    </div>
+
+    <main class="main-content">
+      <div class="wrapper">
+        <div id="faq">FAQ</div>
+        <div class="titlee">Why should we use Tavec?</div>
+        <p class="text">
+        Tavec stands out as an indispensable tool for users seeking seamless and effective financial management. Its all-encompassing suite of features simplifies the complexities of personal finance, offering a user-friendly interface that caters to individuals at any stage of their financial journey. With detailed analytics, Tavec empowers users to take charge of their financial well-being. The platform's commitment to security ensures that sensitive financial data remains protected, while its accessibility across multiple devices and integration with various financial institutions guarantee convenience and ease of use. In essence, Tavec isn't just a website for managing money; it's a comprehensive financial companion dedicated to helping users achieve their financial aspirations with confidence and clarity.
+        </p>
+        <div class="right">
+          <div class="titlee">Who made Tavec?</div>
+          <p class="text">
+          Alan, Vincentius, Keisha, Eric, and Timothy collaborated in creating our website. Our combined expertise, diverse skills, and collective effort resulted in a platform that embodies innovation and efficiency. Each team member brought unique strengths to the table, contributing to various aspects of its development, design, and functionality. Our commitment and dedication are evident in every feature and detail present on the site. Together, we aimed to craft a user-centric experience that prioritizes functionality, accessibility, and reliability. Our collaboration reflects a shared vision to provide users with a cutting-edge tool for effective money management.
+          </p>
+        </div>
+        <br>
+        <div class="gett">
+        <div class="what">What are you waiting for?</div>
+        <div class="join">Join Us Now!</div>
+        </div>
+        <br>
+        <div class="get"><a href="../website/register.php">
+        get started now
+        </a></div>
+        <br><br>
+
+      </div>
+      <div class="footer-basic">
+        <footer>
+            <div class="social">
+              <a href="https://www.instagram.com/ericyyoel/"><i class="icon ion-social-instagram"></i></a>
+              <a href="https://www.youtube.com/@KoboKanaeru"><i class="icon ion-social-youtube"></i></a>
+              <a href="https://www.twitter.com/@SAO_808"><i class="icon ion-social-twitter"></i></a>
+              <a href="https://www.facebook.com/eric.yyoel"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="https://www.petra.ac.id">PCU</a></li>
+                <li class="list-inline-item"><a href="#faq">About</a></li>
+            </ul>
+            <p class="copyright">Tavec © 2023</p>
+        </footer>
+    </div>
+    </main>
+    
+
+  </body>
+</html>
